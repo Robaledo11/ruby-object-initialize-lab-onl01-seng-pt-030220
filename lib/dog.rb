4d170f7419fa7d995e8512fe47167d82ea15fce1
ROBERT  ALEDO <robertaledo22@gmail.com>
2020-03-24 04:03:35 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog 
+  def initialize(name, breed="Mut")
+    @name = name
+    @breed = breed
+    
+def name=(name)
+  @name = name
+end
+def name
+  @name 
+end
+
+
+def breed = (breed)
+  @breed = breed
+ end
+
+def breed 
+  @breed 
+ end
+end
+
+fido = Dog.New("Fido", "Mutt")
+fido.name 
+fido. breed 
---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+Def Person 
+ def initialize (name)
+   @name - name 
+ end
+ 
+ def name = (name)
+   @name = name 
+ end
+ 
+ def name 
+   @name 
+  end
+ end
